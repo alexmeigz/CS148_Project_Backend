9543ddc679d4fc09bab641c83aeaf82ea4bb6827
alexmeigz <alexmei@ucsb.edu>
2020-11-23 01:24:15 +0000

    merge with master

---
+++ migrations/versions/36771da80a63_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 36771da80a63
+Revises: ab79b81257d7, 2699d74c077a
+Create Date: 2020-11-23 01:23:40.441581
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '36771da80a63'
+down_revision = ('ab79b81257d7', '2699d74c077a')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
