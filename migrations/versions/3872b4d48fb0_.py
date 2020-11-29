4236dc731311e901f062dffac1f8868d6ba83a41
sriya112 <sriyaaluru@gmail.com>
2020-11-29 02:26:27 +0000

    merged heads

---
+++ migrations/versions/3872b4d48fb0_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 3872b4d48fb0
+Revises: f683429df188, bf38c530b847
+Create Date: 2020-11-29 02:26:11.616607
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '3872b4d48fb0'
+down_revision = ('f683429df188', 'bf38c530b847')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
